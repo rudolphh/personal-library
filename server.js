b0538a677030de455bd1ae5e5bd64cbba5e11274
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-07-17 13:45:42 -0700

    Add helmet security measures

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 'use strict';
 
+var dotenv      = require('dotenv').load();
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var cors        = require('cors');
+var helmet      = require('helmet');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
 var app = express();
+
+app.use(helmet());
+app.use(helmet.noCache());
+app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -26,9 +32,9 @@ app.route('/')
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
